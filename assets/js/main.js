b4cb912daa7634eb492e4cac7f507fc9a6dc255f
Ernestas Šimkus <ernestas.simk@gmail.com>
2024-02-13 02:09:57 +0200

    Home Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,46 @@
 /*=============== SHOW MENU ===============*/
 
-
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== SHOW CART ===============*/
-
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
 
-
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== SHOW LOGIN ===============*/
-
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
 
-
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== NEW SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== LIGHT BOX ===============*/
 
-
 /*=============== QUESTIONS ACCORDION ===============*/
-
 
 /*=============== STYLE SWITCHER ===============*/
