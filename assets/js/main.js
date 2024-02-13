a5a77e8efdcd94b7cac1e51e21f5213ceff0976d
Ernestas Šimkus <ernestas.simk@gmail.com>
2024-02-13 04:19:13 +0200

    Show Cart, Login & Scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,20 +7,45 @@
 /* Validate if constant exists */
 
 /*=============== SHOW CART ===============*/
+const cart = document.getElementById("cart"),
+  cartShop = document.getElementById("cart-shop"),
+  cartClose = document.getElementById("cart-close");
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
 
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
 
 /*=============== SHOW LOGIN ===============*/
-
+const login = document.getElementById("login"),
+  loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close");
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    login.classList.add("show-login");
+  });
+}
 
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+}
 
 /*=============== HOME SWIPER ===============*/
 var homeSwiper = new Swiper(".home-swiper", {
@@ -50,7 +75,13 @@ var newSwiper = new Swiper(".new-swiper", {
   loop: "true",
 });
 /*=============== SHOW SCROLL UP ===============*/
-
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // when the scroll is higher than 350 viewport height, add the show-scroll class to a tag with the scroll-top class
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 /*=============== LIGHT BOX ===============*/
 
 /*=============== QUESTIONS ACCORDION ===============*/
